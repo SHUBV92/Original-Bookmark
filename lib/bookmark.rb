f0c573304d77301dde4a2982764f44f90dfbdb61
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-11-06 15:49:17 +0000

    Added Database

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,7 +2,7 @@ require 'pg'
 
 class Bookmark
     def self.all
-        connection = PG.connect(dbname: 'bookmark_manager')
+        connection = PG.connect(dbname: ENV['DB_NAME'])
         bookmarks = connection.exec('SELECT * FROM bookmarks')
         bookmarks.map {|bookmark| bookmark['url']}
     end 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 ENV[ 'RACK_ENV' ] = 'test'
 
+ENV['DB_NAME'] = 'bookmark_manager_test'
+
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
